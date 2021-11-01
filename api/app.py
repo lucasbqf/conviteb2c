e05b277936e493d481979c51e0a85618e6f00abd
lucasbqf <lucasbqf@hotmail.com>
2021-11-01 18:24:19 -0300

    put main inside exec

--- api/app.py
+++ api/app.py
@@ -4,29 +4,30 @@ from model.dbConnection import DBConnection
 
 from resources.Invite import InviteClaim, InviteCreator, InvitePaid, Invites, inviteFinish,InviteByUser
 
-app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-api = Api(app)
+def exec():
+    app = Flask(__name__)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    api = Api(app)
 
 
 
-@app.before_first_request
-def criabanco():
-    try:
-        print('criando Banco de dados')
-        dbconnection = DBConnection()
-        dbconnection.createdb()
-    except:
-        print('banco de dados ja existe')
+    @app.before_first_request
+    def criabanco():
+        try:
+            print('criando Banco de dados')
+            dbconnection = DBConnection()
+            dbconnection.createdb()
+        except:
+            print('banco de dados ja existe')
 
-api.add_resource(InviteCreator, '/create_invite')
-api.add_resource(InvitePaid, '/update_invite_paid')
-api.add_resource(InviteClaim, '/update_invite_claim')
-api.add_resource(inviteFinish, '/update_invite_finished')
+    api.add_resource(InviteCreator, '/create_invite')
+    api.add_resource(InvitePaid, '/update_invite_paid')
+    api.add_resource(InviteClaim, '/update_invite_claim')
+    api.add_resource(inviteFinish, '/update_invite_finished')
 
 
-api.add_resource(Invites, '/get_all_invites')
-api.add_resource(InviteByUser, '/get_userinvite=<string:user_id>')
+    api.add_resource(Invites, '/get_all_invites')
+    api.add_resource(InviteByUser, '/get_userinvite=<string:user_id>')
 
 

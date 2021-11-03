bfb47d96228097cadffe9ab6765f1d926915efab
lucasbqf <lucasbqf@hotmail.com>
2021-11-03 18:04:56 -0300

    realizado tratamento de erros para todas as request

--- api/resources/Invite.py
+++ api/resources/Invite.py
@@ -32,8 +32,9 @@ class InvitePaid(Resource):
                 return{"message":"'{}' updated successfully ".format(dados['invited_user_id'])},200
             elif result == 0 :
                 return{"message":"'{}' don't exist or cannot be updated to status 2".format(dados['invited_user_id'])},404
-        except:
-            return {"message": "DB error,failed to execute the required request"},500
+                
+        except Exception as error:
+            return {"message": "Error:{}".format (error)},500
 
 class InviteClaim(Resource):
     """realiza a atualização do status do convite encontrando-o pelo invited_user_id do status 2 para o estatus 3 estatus reinvidicado, pronto para o Administrativo realizar o Pagamento"""
@@ -51,7 +52,7 @@ class InviteClaim(Resource):
             elif result == 0 :
                 return{"message":"'{}' don't exist or cannot be updated to status 3".format(dados['invited_user_id'])},404
         except Exception as error:
-            return {"message": "DB error,failed to execute the required request \n {}".format (error)},500
+            return {"message": "Error:{}".format (error)},500
 
 class inviteFinish(Resource):
     """realiza a atualização do status do convite encontrando-o pelo invited_user_id do status 3 para o estatus 4 estatus Finalizado, onde o valor ja foi reinvidicao pelo usuario e o administrativo realizou o pagamento"""
@@ -69,7 +70,7 @@ class inviteFinish(Resource):
             elif result == 0 :
                 return{"message":"'{}' don't exist or cannot be updated to status 4".format(dados['invited_user_id'])},404
         except Exception as error:
-            return {"message": "DB error,failed to execute the required request \n error:{}".format(error)},500
+            return {"message": "Error:{}".format (error)},500
 
 
 ############----------endpoints para visualização dos convites----------############
@@ -80,46 +81,52 @@ class Invites(Resource):
     path_params = reqparse.RequestParser()
     path_params.add_argument('status')
     def get(self):
-        results = self.dbconnection.getAllInvites()
-        entries = []
-        for result in results:
-            entries.append(
-                {
-                    'invited_user_id':result[0],
-                    'user_id':result[1],
-                    'status':result[2],
-                    'pix_type':result[3],
-                    'pix_info':result[4],
-                    'payer_id':result[5],
-                    'receipt':result[6]
-                }
-            )
-        return{"result": entries },200
+        try:
+            results = self.dbconnection.getAllInvites()
+            entries = []
+            for result in results:
+                entries.append(
+                    {
+                        'invited_user_id':result[0],
+                        'user_id':result[1],
+                        'status':result[2],
+                        'pix_type':result[3],
+                        'pix_info':result[4],
+                        'payer_id':result[5],
+                        'receipt':result[6]
+                    }
+                )
+            return{"result": entries },200
+        except Exception as error:
+            return {"message": "Error:{}".format (error)},500
 
         
     def post(self):
         dados = self.path_params.parse_args()
-        if dados['status'] != None: 
-            if int(dados['status']) > 0 and int(dados['status']) < 5:
-                results = self.dbconnection.getAllInvitesByStatus(dados['status'])
-                entries = []
-                for result in results:
-                    entries.append(
-                        {
-                            'invited_user_id':result[0],
-                            'user_id':result[1],
-                            'status':result[2],
-                            'pix_type':result[3],
-                            'pix_info':result[4],
-                            'payer_id':result[5],
-                            'receipt':result[6]
-                        }
-                    )
-                return{"result": entries },200
+        try:
+            if dados['status'] != None: 
+                if int(dados['status']) > 0 and int(dados['status']) < 5:
+                    results = self.dbconnection.getAllInvitesByStatus(dados['status'])
+                    entries = []
+                    for result in results:
+                        entries.append(
+                            {
+                                'invited_user_id':result[0],
+                                'user_id':result[1],
+                                'status':result[2],
+                                'pix_type':result[3],
+                                'pix_info':result[4],
+                                'payer_id':result[5],
+                                'receipt':result[6]
+                            }
+                        )
+                    return{"result": entries },200
+                else:
+                    return {"message":"a status code is invalid"},400 
             else:
-                return {"message":"a status code is invalid"},400 
-        else:
-            return {"message":"a status code is required"},400
+                return {"message":"a status code is required"},400
+        except Exception as error:
+            return {"message": "Error:{}".format (error)},500
 
 class InviteByUser(Resource):
     """ endpoint que retorna todos os convites de um usuario com GET,
@@ -129,39 +136,45 @@ class InviteByUser(Resource):
     path_params.add_argument('status')
 
     def get(self,user_id):
-        results = self.dbconnection.getAllInvitesByUser(user_id)
-        entries = []
-        for result in results:
-            entries.append(
-                {
-                    'invited_user_id':result[0],
-                    'user_id':result[1],
-                    'status':result[2]
-                }
-            )
-        return{"result": entries },200
+        try:
+            results = self.dbconnection.getAllInvitesByUser(user_id)
+            entries = []
+            for result in results:
+                entries.append(
+                    {
+                        'invited_user_id':result[0],
+                        'user_id':result[1],
+                        'status':result[2]
+                    }
+                )
+            return{"result": entries },200
+        except Exception as error:
+            return {"message": "Error:{}".format (error)},500
 
 
     def post(self,user_id):
         dados = self.path_params.parse_args()
-        #todo fazer o convert de str pra int
-        if dados['status'] != None: 
-            if int(dados['status']) > 0 and int(dados['status']) < 5:
-                results = self.dbconnection.getInvitesByUserAndStatus(user_id,dados['status'])
-                entries = []
-                for result in results:
-                    entries.append(
-                        {
-                            'invited_user_id':result[0],
-                            'user_id':result[1],
-                            'status':result[2]
-                        }
-                    )
-                return{"result": entries },200
+        try:
+            if dados['status'] != None: 
+                if int(dados['status']) > 0 and int(dados['status']) < 5:
+                    results = self.dbconnection.getInvitesByUserAndStatus(user_id,dados['status'])
+                    entries = []
+                    for result in results:
+                        entries.append(
+                            {
+                                'invited_user_id':result[0],
+                                'user_id':result[1],
+                                'status':result[2]
+                            }
+                        )
+                    return{"result": entries },200
+                else:
+                    return {"message":"a status code is invalid"},400 
             else:
-                return {"message":"a status code is invalid"},400 
-        else:
-            return {"message":"a status code is required"},400
+                return {"message":"a status code is required"},400
+
+        except Exception as error:
+            return {"message": "Error:{}".format (error)},500
 
 
 
